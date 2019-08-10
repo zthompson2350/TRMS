17fbbf0c71ddea32ccb42192294b1889bde19d2e
Terry Kow Zhi Im <terryim@Terrys-MacBook-Pro.local>
2019-08-10 15:02:09 -0400

    3:02pm

--- TRMS/src/main/webapp/status.js
+++ TRMS/src/main/webapp/status.js
@@ -1,3 +1,91 @@
+$(document).ready(function() {
+
+
+    // function to make buttons and add to page
+    function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+        $(areaToAddTo).empty();
+
+        for (var i = 0; i < arrayToUse.length; i++) {
+            var a = $("<button>");
+            a.addClass(classToAdd);
+            a.attr("data-type", arrayToUse[i]);
+            a.text(arrayToUse[i]);
+            $(areaToAddTo).append(a);
+        }
+
+    }
+
+    $(document).on("click", ".admin-button", function() {
+        $("#status").empty();
+        $(".admin-button").removeClass("active");
+        $(this).addClass("active");
+
+        var type = $(this).attr("data-type");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+        $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+            .then(function(response) {
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+                    var adminDiv = $("<div class=\"admin-item\">");
+
+                    var rating = results[i].rating;
+
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    var animated = results[i].images.fixed_height.url;
+                    var still = results[i].images.fixed_height_still.url;
+
+                    var adminImage = $("<img>");
+                    adminImage.attr("src", still);
+                    adminImage.attr("data-still", still);
+                    adminImage.attr("data-animate", animated);
+                    adminImage.attr("data-state", "still");
+                    adminImage.addClass("admin-image");
+
+                    adminDiv.append(p);
+                    adminDiv.append(adminImage);
+
+                    $("#status").append(adminDiv);
+                }
+            });
+    });
+
+    $(document).on("click", ".admin-image", function() {
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+
+    $("#add-admin").on("click", function(event) {
+        event.preventDefault();
+        var newadmin = $("input").eq(0).val();
+
+        if (newadmin.length > 2) {
+            status.push(newadmin);
+        }
+
+        populateButtons(status, "admin-button", "#admin-buttons");
+
+    });
+
+    populateButtons(status, "admin-button", "#admin-buttons");
+});
+
+
+
+
 var formUrl = 'http://localhost:8080/TRMS/Status';
 
 
