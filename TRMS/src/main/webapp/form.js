99be7b4c14aa06c1dc41272def5ce70e5e766e7c
unknown <hannfmlerum@gmail.com>
2019-08-11 13:32:54 -0400

    moved login button javascript to its own file, renamed all calls to it

--- TRMS/src/main/webapp/form.js
+++ TRMS/src/main/webapp/form.js
@@ -46,4 +46,5 @@ function jsonBuilder() {
 window.onload = function() {
     console.log("immediate invoked onload");
     document.getElementById("input").addEventListener("click", postForm, false);
+    setLoginButton();
 }
