6c2ba01a101c207b905f2bf6fdb553b5d53228ee
zthompson2350 <ZAT0002@auburn.edu>
2019-08-11 13:45:11 -0400

    added doGets to every servlet except login

---
+++ TRMS/src/main/java/com/revature/servlet/NewsServlet.java
@@ -0,0 +1,33 @@
+package com.revature.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class NewsServlet
+ */
+public class NewsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String username = (String)session.getAttribute("name");
+		String usernameJSON = "\"username\" : \"" + username + "\"";
+		PrintWriter out = response.getWriter();
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		out.print(usernameJSON);
+		out.flush();
+		out.close();
+	}
+
+}
